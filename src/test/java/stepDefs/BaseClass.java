99e8560634f6e539116f2751d95e0bf251188276
Satyabrata2000 <satyabrata2000@gmail.com>
2022-04-09 02:23:45 -0500

    Cucumber Project

---
+++ src/test/java/stepDefs/BaseClass.java
@@ -0,0 +1,36 @@
+package stepDefs;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class BaseClass {
+	
+	public static WebDriver driver;
+	
+	@Before
+	public void SetUp() {
+		
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        //System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+
+        driver = new ChromeDriver();
+       // WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.simplilearn.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+		
+	}
+	
+	@After
+	public void TearDown() {
+		
+		driver.close();
+		
+	}
+
+}
